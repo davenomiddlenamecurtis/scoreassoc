53028843229308e924cef903ec68132de423cf2c
Dave Curtis <d.curtis@ucl.ac.uk>
2016-05-27 12:36:38 +0100

    Update runsa.sh

--- runsa.sh
+++ runsa.sh
@@ -18,5 +18,5 @@ GENE=$1
 
 pseq ${PROJECT} v-view --geno  --phenotype ${PHENO} --gene $GENE > ${GENE}.dat
 pseq ${PROJECT} counts --annotate refseq --gene $GENE --phenotype ${PHENO} > ${GENE}.annot
-scoreassoc –psdatafile ${GENE}.dat --annotfile ${GENE}.annot --weightfile funcWeights.txt --filterfile exclusions.txt --outfile ${GENE}.psao --minweight 80 --ldthreshold 0.7 --dorecessive
+scoreassoc –psdatafile ${GENE}.dat --annotfile ${GENE}.annot --weightfile funcWeights.txt --filterfile exclusions.txt --outfile ${GENE}.psao --minweight 80 --ldthreshold 0.7 --dorecessive 1
 
