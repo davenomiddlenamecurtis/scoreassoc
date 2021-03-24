51fc4137cce58163d70fffa437c2ba773ca60d00
Dave Curtis <davenomiddlenamecurtis@gmail.com>
2021-03-24 14:44:31 +0000

    Multiple weights

--- src/saglobals.cpp
+++ src/saglobals.cpp
@@ -20,9 +20,9 @@ along with scoreassoc.If not, see <http://www.gnu.org/licenses/>.
 #include "scoreassoc.hpp"
 
 // lazily make these global to save allocating them and to avoid stack overflow
-float missing_score[MAX_LOCI],cc_freq[2][MAX_LOCI],cc_count[2][MAX_LOCI],cc_genocount[2][3][MAX_LOCI];
-double weight[MAX_LOCI], func_weight[MAX_LOCI];
-int rarer[MAX_LOCI],max_cc[2];
+float **missing_score,cc_freq[2][MAX_LOCI],cc_count[2][MAX_LOCI],cc_genocount[2][3][MAX_LOCI];
+double **weight, **func_weight;
+int rarer[MAX_LOCI],max_cc[2],numScores;
 char names[MAX_LOCI][LOCUS_NAME_LENGTH],comments[MAX_LOCI][MAX_COMMENT_LENGTH],trios_fn[500];
 lrVariable allVars[MAXLRVARIABLES];
 std::map<std::string,lrVariable *> varMap;
